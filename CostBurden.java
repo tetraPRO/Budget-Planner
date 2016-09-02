6888f142f5ec13cd70e92c6e1e32a1999a9ca4c3
Philip Caputo <caputo.philip@gmail.com>
2016-09-02 07:21:39 -0700

    Update CostBurden.java

--- CostBurden.java
+++ CostBurden.java
@@ -4,14 +4,6 @@ import java.math.RoundingMode;
 import javax.swing.JFrame;
 import net.proteanit.sql.DbUtils;
 
-
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author capphil1
